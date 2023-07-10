8b0c01d39940a461d68622badb0db34e416325b3
MazereAndreea <MazereAndreea@users.noreply.github.com>
2023-07-10 15:30:28 +0300

    Worked on the form. Now I can: open the popup form, make the rest of the page blurred out, close the form and unblur the page. Still can't figured out how to close the form by clicking anywhere on the blurred page

--- scripts/form.js
+++ scripts/form.js
@@ -6,22 +6,17 @@ $(function(){
     const button2 = $(".closeForm");
     
     $(button1).on("click", function(){
+        $('body > *:not(#form)').css("pointer-events", "none"); //make the user unable to interact with the blurred part
         form.css('display', 'block'); //display the form
         var formPosition = $(form).offset(); //get the position of form (top and left)
         var formHeight = $(form).height(); //get the height of the form
         $('body > *:not(#form)').css('filter', 'blur(5px)'); //blur the rest of the page
-        $('html,body').animate({
+        $('html').animate({
            scrollTop: formPosition.top - formHeight/2 //scrolltop counts from the top of the page to the bottom
         }, 500);
     });
     
     $(button2).on("click", function(){
-        $('body > *:not(#form)').css("pointer-events", "none");
-        form.css('display', 'none'); //hide form
-        $('body > *:not(#form)').css('filter', 'none'); //unblur page
-    });
-
-    $('body > *').on("touchstart", function(){
         $('body > *:not(#form)').css("pointer-events", "all");
         form.css('display', 'none'); //hide form
         $('body > *:not(#form)').css('filter', 'none'); //unblur page
