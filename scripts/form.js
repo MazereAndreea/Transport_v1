cd2a43a4d496dae12847dcc45a580c6bbed2ccb2
MazereAndreea <MazereAndreea@users.noreply.github.com>
2023-07-10 15:16:49 +0300

    Still working on the form but I want to save some progress

--- scripts/form.js
+++ scripts/form.js
@@ -1,22 +1,34 @@
-// for blurring the rest of the page and display form
 
 $(function(){
 
-    const button = $(".openForm");
+    const button1 = $(".openForm");
     const form = $("#form");
-
-    button.click(function()
-    {
-        form.css('display','block');
-        $('body > *:not(#form)').css('filter', 'blur(5px)');
-        
+    const button2 = $(".closeForm");
+    
+    $(button1).on("click", function(){
+        form.css('display', 'block'); //display the form
+        var formPosition = $(form).offset(); //get the position of form (top and left)
+        var formHeight = $(form).height(); //get the height of the form
+        $('body > *:not(#form)').css('filter', 'blur(5px)'); //blur the rest of the page
+        $('html,body').animate({
+           scrollTop: formPosition.top - formHeight/2 //scrolltop counts from the top of the page to the bottom
+        }, 500);
     });
+    
+    $(button2).on("click", function(){
+        $('body > *:not(#form)').css("pointer-events", "none");
+        form.css('display', 'none'); //hide form
+        $('body > *:not(#form)').css('filter', 'none'); //unblur page
+    });
+
+    $('body > *').on("touchstart", function(){
+        $('body > *:not(#form)').css("pointer-events", "all");
+        form.css('display', 'none'); //hide form
+        $('body > *:not(#form)').css('filter', 'none'); //unblur page
+    });
+
 });
 
-// for escaping form touching a button
-function closeForm() {
-    document.getElementById("form").style.display = "none";
-}
 
-// for escaping form touching outside of it
+
 
