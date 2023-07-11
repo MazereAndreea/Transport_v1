37ed277c01444cd2d46c9c89b527359b4b75844f
MazereAndreea <MazereAndreea@users.noreply.github.com>
2023-07-11 20:48:49 +0300

    Trying to use js to change the select element for states depending on what country.

--- scripts/option-values.js
+++ scripts/option-values.js
@@ -1,11 +1,39 @@
 import data from '/scripts/country-states.json' assert {type: 'json'};
 
-var select = document.getElementById("incarcare");
+var select1 = document.getElementById("load-country");
+var select2 = document.getElementById("unload-country");
 var countries = data.countries;
 
 for(let i in countries){
-    var option = document.createElement("option");
-    option.text = countries[i].country;
-    option.value = countries[i].country;
-    select.appendChild(option);
+    //for first select
+    var option1 = document.createElement("option");
+    option1.text = countries[i].country;
+    option1.value = countries[i].country;
+    select1.appendChild(option1);
+    //for second select
+    var option2 = document.createElement("option");
+    option2.text = countries[i].country;
+    option2.value = countries[i].country;
+    select2.appendChild(option2);
 }
+
+//for loading cities based on country selected
+
+var select3 = document.getElementById("load-city");
+var select4 = document.getElementById("unload-city");
+const select = document.getElementById("load-country");
+
+select.addEventListener("change", function(){
+    const where = (element) => element = this.value;
+    console.log(countries.findIndex(where));
+});
+
+
+    // for(let i in list_of_states){
+
+    //     var option = document.createElement("option");
+    //     option.text = countries[where].states[i];
+    //     option.value = countries[i].country;
+    //     select3.appendChild(option);
+ 
+
